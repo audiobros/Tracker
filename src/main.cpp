2ff309eaa1f2985aa364b1fbda056c1495305f28
Sercan Atlı <hsercanatli-air@Sercans-MacBook-Air.local>
2023-03-13 19:10:25 +0100

    Initial work for cmake to build tracktion statically

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
-int main(int argc, char** argv){
-    std::cout << "Hello World" << std::endl;
-    return 1;
+#include <tracktion_engine/tracktion_engine.h>
+
+
+int main(){
+  std::unique_ptr<juce::ScopedJuceInitialiser_GUI> initializer = std::make_unique<juce::ScopedJuceInitialiser_GUI>();
+  std::unique_ptr<tracktion::Engine> engine = std::make_unique<tracktion::Engine>("TrackerBro");
+
+  std::cout << "Here!, congrats!" << std::endl;
+
+  return 0;
 }
