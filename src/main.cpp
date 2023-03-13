5558678ede5dbf83f48dd80f23b405adba7654d6
Sercan Atlı <hsercanatli-air@Sercans-MacBook-Air.local>
2023-03-13 20:59:51 +0100

    Trying to initialize edit on main cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,13 @@
 int main(){
   std::unique_ptr<juce::ScopedJuceInitialiser_GUI> initializer = std::make_unique<juce::ScopedJuceInitialiser_GUI>();
   std::unique_ptr<tracktion::Engine> engine = std::make_unique<tracktion::Engine>("TrackerBro");
+  std::unique_ptr<tracktion::Edit> edit;
+
+  const auto editFilePath = juce::JUCEApplication::getCommandLineParameters().replace ("-NSDocumentRevisionsDebugMode YES", "").unquoted().trim();
+  const juce::File editFile (editFilePath);
+
+  auto f = juce::File::createTempFile (".ogg");
 
   std::cout << "Here!, congrats!" << std::endl;
-
   return 0;
 }
